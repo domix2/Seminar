cbe238f2c117e330a91e5f0cd49016a7c0e3cf18
Domagoj <domagoj.speljko@gmail.com>
2023-07-04 12:23:10 +0200

    Napravljen rastav programskog koda

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ import os
 import time
 import random
 import sqlite3 
+from igrica.utillities import collide
+from igrica.laser import Laser
+from igrica import Enemy, Player, Ship
 
 pygame.font.init()
 
@@ -35,8 +38,6 @@ YELLOW_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_yellow.png"
 # Učitavanje pozadinske slike
 Background = pygame.transform.scale(pygame.image.load(os.path.join("assets", "background-black.png")), (WIDTH, HEIGHT))
 
-
-
 def save_name(player_name,score,level):
     c.execute("SELECT COUNT(*) FROM players")
     row_count = c.fetchone()[0]
@@ -55,128 +56,6 @@ def save_name(player_name,score,level):
     pygame.display.update()
     time.sleep(2)
     return player_id
-class Laser:
-    def __init__(self, x, y, img):
-        self.x = x
-        self.y = y
-        self.img = img
-        self.mask = pygame.mask.from_surface(self.img)
-
-    def draw(self, window):
-        window.blit(self.img, (self.x, self.y))
-
-    def move(self, vel):
-        self.y += vel
-
-    def off_screen(self, height):
-        return not(self.y <= height and self.y >= 0)
-
-    def collision(self, obj):
-        return collide(self, obj)
-class Ship:
-    COOLDOWN = 30
-    def __init__(self, x, y, health= 100):
-        self.x = x
-        self.y = y
-        self.health = health
-        self.ship_img = None
-        self.laser_img = None
-        self.lasers = []
-        self.cool_down_counter = 0
-
-    def draw(self, window):
-        window.blit(self.ship_img, (self.x, self.y))
-        for laser in self.lasers:
-            laser.draw(WINDOW)
-
-    def move_lasers(self, vel, obj):
-        self.cooldown()
-        for laser in self.lasers:
-            laser.move(vel)
-            if laser.off_screen(HEIGHT):
-                self.lasers.remove(laser)
-            elif laser.collision(obj):
-                obj.health -= 10
-                self.lasers.remove(laser)
-    def cooldown(self):
-        if self.cool_down_counter >= self.COOLDOWN:
-            self.cool_down_counter = 0
-        elif self.cool_down_counter > 0:
-            self.cool_down_counter +=1
-    def shoot(self):
-        if self.cool_down_counter == 0:
-            laser = Laser(self.x, self.y, self.laser_img)
-            self.lasers.append(laser)
-            self.cool_down_counter = 1
-    def get_width(self):
-        return self.ship_img.get_width()
-
-    def get_height(self):
-        return self.ship_img.get_height()
-class Player(Ship):
-    def __init__(self, x, y, health = 100):
-        super().__init__(x,y, health)
-        self.ship_img = YELLOW_SPACE_SHIP
-        self.laser_img = YELLOW_LASER
-        self.mask = pygame.mask.from_surface(self.ship_img)
-        self.max_health = health
-        self.score = 0
-
-    def move_lasers(self, vel, enemies):
-        self.cooldown()
-        for laser in self.lasers:
-            laser.move(vel)
-            if laser.off_screen(HEIGHT):
-                self.lasers.remove(laser)
-            else:
-                for enemy in enemies[:]:
-                    if laser.collision(enemy):
-                        enemies.remove(enemy)
-                        self.lasers.remove(laser)
-                        self.score += 1
-                        break
-
-    def  draw(self, window):
-        super().draw(window)
-        self.healthbar(window)
-        self.scoreboard(window)
-
-    def scoreboard(self, window):
-        font = pygame.font.SysFont("Arial", 30)
-        score_label = font.render("Score: " + str(self.score), 1, (255, 255, 255))
-        window.blit(score_label, (10, 50))
-
-    def healthbar(self, window):
-        pygame.draw.rect(window, (255,0,0), (self.x, self.y + self.ship_img.get_height() + 10, self.ship_img.get_width()
-                                             ,10))
-        pygame.draw.rect(window, (0, 255, 0),
-                         (self.x, self.y + self.ship_img.get_height() + 10, self.ship_img.get_width()* (self.health
-                                                                                                        /self.max_health)
-                          , 10))
-class Enemy(Ship):
-    COLOR_MAP = {"red": (RED_SPACE_SHIP, RED_LASER),
-                 "green": (GREEN_SPACE_SHIP, GREEN_LASER),
-                 "blue": (BLUE_SPACE_SHIP, BLUE_LASER)
-                 }
-
-    def __init__(self, x, y, color, health = 100):
-        super().__init__(x, y, health)
-        self.ship_img, self.laser_img = self.COLOR_MAP[color]
-        self.mask = pygame.mask.from_surface(self.ship_img)
-
-    def move(self, vel):
-        self.y += vel
-
-    def shoot(self):
-        if self.cool_down_counter == 0:
-            laser = Laser(self.x-15, self.y, self.laser_img)
-            self.lasers.append(laser)
-            self.cool_down_counter = 1
-
-def collide(obj1, obj2):
-    offset_x = obj2.x - obj1.x
-    offset_y = obj2.y - obj1.y
-    return obj1.mask.overlap(obj2.mask, (offset_x, offset_y)) != None
 
 def game_over(player,level):
     run = True
